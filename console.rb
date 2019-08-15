41d815fcd2d6244f60acdcf48b0eb8ee64a5fe3d
Joe Cooper <josephcooper3@gmail.com>
2019-08-15 14:50:18 +0100

    created Star class with methods

--- console.rb
+++ console.rb
@@ -1,14 +1,23 @@
 require_relative('./models/movie')
+require_relative('./models/star')
+
 require('pry')
 
 Movie.delete_all()
+Star.delete_all()
 
 movie1 = Movie.new({
   "title" => "Brassed Off",
-  "genre" => "Black Comedy",
+  "genre" => "Comedy-Drama"
 })
-
 movie1.save()
+
+
+star1 = Star.new({
+  "first_name" => "Ewan",
+  "last_name" => "McGregor"
+})
+star1.save()
 
 binding.pry
 nil
--- models/star.rb
+++ models/star.rb
@@ -1,3 +1,27 @@
+require_relative('../db/sql_runner')
+
 class Star
+
+  attr_reader :id
+  attr_accessor :first_name, :last_name
+
+  def initialize(options)
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @id = options['id'].to_i if options['id']
+  end
+
+  def save()
+    sql = "INSERT INTO stars (first_name, last_name)
+    VALUES ($1, $2)
+    RETURNING id"
+    values = [@first_name, @last_name]
+    @id = SqlRunner.run(sql, values).first["id"].to_i()
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM stars"
+    SqlRunner.run(sql)
+  end
 
 end
